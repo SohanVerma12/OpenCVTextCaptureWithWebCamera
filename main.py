a7740c9bd1c093c13566f0456d24aab5dcf9e1d6
GovindaRadha <sohansinghv55@gmail.com>
2023-03-12 23:47:18 +0530

    sohan verma

---
+++ main.py
@@ -0,0 +1,50 @@
+# import cv2
+# from PIL import Image
+# from pytesseract import pytesseract
+#
+# # from cv2 import VideoCapture
+# # from cv2 import waitKey   pip install opencv-python==3.4.17.61
+#
+# camera = cv2.VideoCapture(0)
+#
+# while True:
+#     _,Image = camera.read()
+#     cv2.imshow('Text detection', Image)
+#     if cv2.waitKey(1) & 0xFF == ord('s'):
+#         cv2.imwrite('test1.jpg', Image)
+#         break
+# camera.release()
+# cv2.destroyAllWindows()
+#
+#
+# def tesseract():
+#     path_to_tesseract = "C:\Program Files\Tesseract-OCR\tesseract.exe"
+#     Imagepath = 'test1.jpg'
+#     pytesseract.tesseract_cmd = path_to_tesseract
+#     text = pytesseract.image_to_string(Image.open(Imagepath))
+#     print(text[:1])
+#
+#
+# tesseract()
+
+import cv2
+from PIL import Image
+from pytesseract import pytesseract
+camera=cv2.VideoCapture(0)
+
+while True:
+    _,image=camera.read()
+    cv2.imshow('image',image)
+    if cv2.waitKey(1)& 0xFF==ord('s'):
+        cv2.imwrite('test1.jpg',image)
+        break
+camera.release()
+cv2.destroyAllWindows()
+
+def tesseract():
+    path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    image_path = "test1.jpg"
+    pytesseract.tesseract_cmd = path_to_tesseract
+    text = pytesseract.image_to_string(Image.open(image_path))
+    print(text[:-1])
+tesseract()
